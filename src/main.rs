818f509776233f87c9621e52e43fb7b3110146ea
Gleb Kiselev <g.kiselev@examus.com>
2024-09-16 19:10:20 +0200

    fix: import

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use translate::SerbianTranslation;
-
-mod translate;
-
-fn main() {
-    println!("{}", SerbianTranslation::from_latin("njegov čaj"));
-    println!("{}", SerbianTranslation::from_cyrillic("његов чај"));
-}
